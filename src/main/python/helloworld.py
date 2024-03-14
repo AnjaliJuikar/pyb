412d64fc4d54be9943bc600dcb923c2a70c39162
AnjaliJuikar <anjali.juikar@gmail.com>
2024-03-14 13:53:35 +0530

    Changed HelloWorld.py code

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -3,13 +3,14 @@ This is a simple module to demonstrate a hello world function in
 Python.
 """
 import sys
-def helloworld(out):
-   """
-    Print a hello world message.
-    Args:
-        out: Output stream to write the message to.
-   """
 
-   out.write("Hello world of Python Anjali\n")
+def helloworld(out):
+    """
+    Print a hello world message.
+    Args:
+    out: Output stream to write the message to.
+    """
+
+    out.write("Hello world of Python Anjali\n")
 # Call the function with a file object
-helloworld(sys.stdout)
+    helloworld(sys.stdout)
